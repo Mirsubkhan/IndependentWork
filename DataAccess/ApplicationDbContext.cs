7ddfedfdd6b435f1921670ca2b36157761e1bdc9
pc2 <pc2@DESKTOP-ORGCS6K>
2024-10-17 19:18:51 +0300

    Добавьте файлы проекта.

---
+++ DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): DbContext(options)
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<WalletItem> WalletItems { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<CryptoAsset> CryptoAssets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //
+        }
+
+    }
+}
---
+++ DataAccess/WalletItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public class WalletItem
+    {
+        public Guid Id { get; set; }
+        public Guid WalletId { get; set; }
+        public Guid CryptoAssetId { get; set; }
+        
+    }
+}
